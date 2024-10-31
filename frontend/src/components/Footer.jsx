54fb0bc95aa766129a5ef3eba5f50f18d34ea656
dhanyaravi30 <dhanyafab@gmail.com>
2024-10-31 14:32:46 +0530

    Done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
 		<footer className='py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800'>
 			<div className='flex flex-col items-center justify-center gap-4 md:h-24'>
 				<p className='text-center text-sm leading-loose text-muted-foreground'>
-					Copyright © {new Date().getFullYear()} Dhanya K R. A Netflix clone using MERN Stack.
+					Copyright © {new Date().getFullYear()} Dhanya K R.
 				</p>
 			</div>
 		</footer>
